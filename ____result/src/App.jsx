b536650ea753aa557c599c0504a3f3b646b2c48b
Leonid Petrov1 <leonid_petrov1@epam.com>
2022-01-22 23:44:08 +0400

    Remove unused files and npm dependencies

--- ____result/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Container from "./Container";
-import ErrorBoundary from "./ErrorBoundary";
-
-const App = () => (
-    <ErrorBoundary><Container /></ErrorBoundary>
-);
-
-export default App;
